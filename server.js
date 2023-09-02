802c172132c486db6227b26631ce773035b0d0e5
Nihar4 <niharpatel4444@gmail.com>
2023-09-02 10:27:56 +0530

    change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connectDB();
 
 let part1, part2, part3, part4;
 const getsymbols = async () => {
-    const response = await axios.get('http://localhost:4000/api/v1/splitallstocks');
+    const response = await axios.get('https://portfolio-x-two.vercel.app/api/v1/splitallstocks');
     part1 = response.data.part1;
     part2 = response.data.part2;
     part3 = response.data.part3;
@@ -17,47 +17,47 @@ const getsymbols = async () => {
 getsymbols();
 
 let part1data = [], part2data = [], part3data = [], part4data = [];
-nodeCron.schedule("0 44 */2 * * *", async () => {
+nodeCron.schedule("0 30 */2 * * *", async () => {
     try {
         console.log(part1);
-        part1data = await axios.post('http://localhost:4000/api/v1/createallstocks', { "part": part1 });
+        part1data = await axios.post('https://portfolio-x-two.vercel.app/api/v1/createallstocks', { "part": part1 });
         console.log(part1data)
     } catch (error) {
         console.log(error);
     }
 });
-nodeCron.schedule("0 30 */2 * * *", async () => {
+nodeCron.schedule("0 35 */2 * * *", async () => {
     try {
         console.log(part2)
-        part2data = await axios.post('http://localhost:4000/api/v1/createallstocks', { "part": part2 });
+        part2data = await axios.post('https://portfolio-x-two.vercel.app/api/v1/createallstocks', { "part": part2 });
 
     } catch (error) {
         console.log(error);
     }
 });
-nodeCron.schedule("0 0 */3 * * *", async () => {
+nodeCron.schedule("0 40 */2 * * *", async () => {
     try {
         console.log("hello")
-        part3data = await axios.post('http://localhost:4000/api/v1/createallstocks', { "part": part3 });
+        part3data = await axios.post('https://portfolio-x-two.vercel.app/api/v1/createallstocks', { "part": part3 });
 
     } catch (error) {
         console.log(error);
     }
 });
-nodeCron.schedule("0 30 */3 * * *", async () => {
+nodeCron.schedule("0 45 */2 * * *", async () => {
     try {
         console.log("hello")
-        part4data = await axios.post('http://localhost:4000/api/v1/createallstocks', { "part": part4 }).filteredLogoData;
+        part4data = await axios.post('https://portfolio-x-two.vercel.app/api/v1/createallstocks', { "part": part4 }).filteredLogoData;
 
     } catch (error) {
         console.log(error);
     }
 });
 
-nodeCron.schedule("0 58 */3 * * *", async () => {
+nodeCron.schedule("0 50 */2 * * *", async () => {
     try {
         console.log("hello")
-        await axios.post('http://localhost:4000/api/v1/mergeAllStocks', { part1data, part2data, part3data, part4data });
+        await axios.post('https://portfolio-x-two.vercel.app/api/v1/mergeAllStocks', { part1data, part2data, part3data, part4data });
 
     } catch (error) {
         console.log(error);
